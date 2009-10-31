c74a58aa64a25094cd0d4bace3086b0441f3971f
Michael Seifert <mseifert@error-reports.org>
2009-10-31 15:26:13 +0000

    Optimized drawing routine

--- src/openjchart/charts/PieChart.java
+++ src/openjchart/charts/PieChart.java
@@ -19,6 +19,7 @@ public class PieChart extends Chart {
 
 	private DataTable data;
 	private double degreesPerValue;
+	private double[] startValues;
 
 	public PieChart(DataTable data) {
 		setSettingDefault(KEY_RADIUS, 1.0);
@@ -44,19 +45,12 @@ public class PieChart extends Chart {
 		double h = getHeight() - insets.top - insets.bottom;
 		double size = Math.min(w, h) * this.<Double>getSetting(KEY_RADIUS);
 		g2d.translate(getWidth()/2d, getHeight()/2d);
-		double angleStart = getSetting(KEY_START);
-		double angleStop = angleStart;
+		startValues[0] = getSetting(KEY_START);
+		startValues[startValues.length-1] = Math.signum(degreesPerValue) * 360.0 + startValues[0];
 		ColorMapper colorList = getSetting(KEY_COLORS);
-		for (int i = 0; i < data.getRowCount();  i++) {
-			double val = data.get(0, i).doubleValue();
-			// Ignore negative values
-			if (val <= 0.0) {
-				continue;
-			}
-			angleStop = data.get(0, i).doubleValue() * degreesPerValue;
-			g2d.setColor(colorList.get(i/(double)data.getRowCount()));
-			g2d.fill(new Arc2D.Double(-size/2d, -size/2d, size, size, angleStart, angleStop, Arc2D.PIE));
-			angleStart += angleStop;
+		for (int i = 1; i < startValues.length;  i++) {
+			g2d.setColor(colorList.get(i-1/(double)startValues.length));
+			g2d.fill(new Arc2D.Double(-size/2d, -size/2d, size, size, startValues[i-1], startValues[i]-startValues[i-1], Arc2D.PIE));
 		}
 		g2d.setTransform(txOld);
 		g2d.setColor(colorOld);
@@ -65,7 +59,7 @@ public class PieChart extends Chart {
 	@Override
 	public void dataChanged(DataTable data) {
 		super.dataChanged(data);
-		
+
 		// Calculate sum of all values
 		double colYSum = 0.0;
 		for (int i = 0; i < data.getRowCount();  i++) {
@@ -82,6 +76,12 @@ public class PieChart extends Chart {
 		}
 		else {
 			degreesPerValue = 360.0/colYSum;
+		}
+
+		// Calculate starting angles
+		startValues = new double[data.getRowCount()+1];
+		for (int i = 1; i < data.getRowCount(); i++) {
+			startValues[i] = startValues[i-1] + data.get(0, i-1).doubleValue() * degreesPerValue;
 		}
 	}
 }
