1126eb138c14622a1bd6ddccab6a379304b99b0f
Michael Seifert <mseifert@error-reports.org>
2009-12-26 18:51:04 +0000

    Changed TODO.txt; Added copyright and LGPL license texts

--- src/openjchart/plots/shapes/AbstractShapeRenderer.java
+++ src/openjchart/plots/shapes/AbstractShapeRenderer.java
@@ -1,3 +1,23 @@
+/* OpenJChart : a free plotting library for the Java(tm) platform
+ *
+ * (C) Copyright 2009, by Erich Seifert and Michael Seifert.
+ *
+ * This file is part of OpenJChart.
+ *
+ * OpenJChart is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OpenJChart is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenJChart.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package openjchart.plots.shapes;
 
 import java.awt.Color;
