2bd61e209c16a831226f36864bb5bca33c08b95a
Erich Seifert <dev@erichseifert.de>
2010-02-07 19:06:30 +0000

    Added GPL license text; Added Maven license plugin to POM; Updated license texts; Removed target directory from SVN

--- src/main/java/de/erichseifert/gral/data/comparators/Ascending.java
+++ src/main/java/de/erichseifert/gral/data/comparators/Ascending.java
@@ -1,20 +1,21 @@
-/* GRAL : a free graphing library for the Java(tm) platform
+/**
+ * GRAL : Vector export for Java(R) Graphics2D
  *
- * (C) Copyright 2009-2010, by Erich Seifert and Michael Seifert.
+ * (C) Copyright 2010 Erich Seifert <info[at]erichseifert.de>, Michael Seifert <michael.seifert[at]gmx.net>
  *
  * This file is part of GRAL.
  *
  * GRAL is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * GRAL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * Lesser GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Lesser General Public License
  * along with GRAL.  If not, see <http://www.gnu.org/licenses/>.
  */
 
