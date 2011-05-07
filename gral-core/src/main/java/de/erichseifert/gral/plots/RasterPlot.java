4a7943e73b95c45703c1039081db9a095017e348
Erich Seifert <dev@erichseifert.de>
2011-05-07 16:13:17 +0000

    Added simple implementation of a raster plot

---
+++ gral-core/src/main/java/de/erichseifert/gral/plots/RasterPlot.java
@@ -0,0 +1,199 @@
+/*
+ * GRAL: GRAphing Library for Java(R)
+ *
+ * (C) Copyright 2009-2011 Erich Seifert <dev[at]erichseifert.de>,
+ * Michael Seifert <michael.seifert[at]gmx.net>
+ *
+ * This file is part of GRAL.
+ *
+ * GRAL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GRAL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GRAL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.erichseifert.gral.plots;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Rectangle2D;
+
+import de.erichseifert.gral.AbstractDrawable;
+import de.erichseifert.gral.Drawable;
+import de.erichseifert.gral.DrawingContext;
+import de.erichseifert.gral.data.DataSource;
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.data.Row;
+import de.erichseifert.gral.data.statistics.Statistics;
+import de.erichseifert.gral.plots.axes.Axis;
+import de.erichseifert.gral.plots.axes.AxisRenderer;
+import de.erichseifert.gral.plots.colors.ColorMapper;
+import de.erichseifert.gral.plots.colors.Grayscale;
+import de.erichseifert.gral.plots.points.AbstractPointRenderer;
+import de.erichseifert.gral.plots.points.PointRenderer;
+import de.erichseifert.gral.util.GraphicsUtils;
+import de.erichseifert.gral.util.PointND;
+
+
+/**
+ * <p>Class that displays the values in the columns and rows of a data source
+ * as a raster of boxes.</p>
+ * <p>To create a new <code>RasterPlot</code> simply create a new instance using
+ * a data source. Example:</p>
+ * <pre>
+ * DataTable data = new DataTable(Double.class, Double.class);
+ * data.add(10.98, -12.34);
+ * data.add( 7.65,  45.67);
+ * data.add(43.21,  89.01);
+ *
+ * RasterPlot plot = new RasterPlot(data);
+ * </pre>
+ */
+public class RasterPlot extends XYPlot {
+	/** Key for specifying a {@link java.awt.Point2D} instance which defines
+	the horizontal and vertical offset of the raster from the origin. */
+	public static final Key OFFSET =
+		new Key("rasterplot.offset"); //$NON-NLS-1$
+	/** Key for specifying a {@link java.awt.Dimension2D} instance which
+	defines the horizontal and vertical distance of the raster pixels. */
+	public static final Key DISTANCE =
+		new Key("rasterplot.distance"); //$NON-NLS-1$
+	/** Key for specifying an instance of
+	{@link de.erichseifert.gral.plots.colors.ColorMapper} used for mapping the
+	pixel values to colors. */
+	public static final Key COLORS =
+		new Key("rasterplot.color"); //$NON-NLS-1$
+
+	/**
+	 * Class that renders a box and its whiskers in a box-and-whisker plot.
+	 */
+	protected static class RasterRenderer extends AbstractPointRenderer {
+		/** Bar plot this renderer is associated to. */
+		private final RasterPlot plot;
+
+		/**
+		 * Constructor that creates a new instance and initializes it with a
+		 * plot as data provider.
+		 * @param plot Data provider.
+		 */
+		public RasterRenderer(RasterPlot plot) {
+			this.plot = plot;
+		}
+
+		/**
+		 * Returns the graphical representation to be drawn for the specified data
+		 * value.
+		 * @param axis that is used to project the point.
+		 * @param axisRenderer Renderer for the axis.
+		 * @param row Data row containing the point.
+		 * @return Component that can be used to draw the point
+		 */
+		public Drawable getPoint(final Axis axisY,
+				final AxisRenderer axisYRenderer, final Row row) {
+			//final Drawable plotArea = BarPlot.this.plotArea;
+			return new AbstractDrawable() {
+				private final Rectangle2D pixel = new Rectangle2D.Double();
+				private final Axis axisX = plot.getAxis(AXIS_X);
+				private final Axis axisY = plot.getAxis(AXIS_Y);
+				private final AxisRenderer axisXRenderer = plot.getAxisRenderer(AXIS_X);
+				private final AxisRenderer axisYRenderer = plot.getAxisRenderer(AXIS_Y);
+
+				public void draw(DrawingContext context) {
+					double valueX = row.get(0).doubleValue();
+					double valueY = row.get(1).doubleValue();
+					double value = row.get(2).doubleValue();
+
+					// Pixel dimensions
+					double xMin = axisXRenderer
+						.getPosition(axisX, valueX - 0.5, true, false)
+						.get(PointND.X);
+					double xMax = axisXRenderer
+						.getPosition(axisX, valueX + 0.5, true, false)
+						.get(PointND.X);
+					double width = Math.abs(xMax - xMin) + 1.0;
+					double yMin = axisYRenderer
+						.getPosition(axisY, valueY - 0.5, true, false)
+						.get(PointND.Y);
+					double yMax = axisYRenderer
+						.getPosition(axisY, valueY + 0.5, true, false)
+						.get(PointND.Y);
+					double height = Math.abs(yMax - yMin) + 1.0;
+
+					// Create shapes
+					// The origin of all shapes is (boxX, boxY)
+					pixel.setFrame(0.0, 0.0, width, height);
+
+					// Paint pixel
+					Graphics2D graphics = context.getGraphics();
+					ColorMapper colorMapper = plot.getSetting(COLORS);
+					Color color = colorMapper.get(value);
+					GraphicsUtils.fillPaintedShape(
+							graphics, pixel, color, pixel.getBounds2D());
+				}
+			};
+		}
+
+		/**
+		 * Returns a <code>Shape</code> instance that can be used
+		 * for further calculations.
+		 * @param row Data row containing the point.
+		 * @return Outline that describes the point's shape.
+		 */
+		public Shape getPointPath(Row row) {
+			return null;
+		}
+	}
+
+	/**
+	 * Initializes a new box-and-whisker plot with the specified data source.
+	 * @param data Data to be displayed.
+	 */
+	public RasterPlot(DataSource data) {
+		setSettingDefault(COLORS, new Grayscale());
+
+		getPlotArea().setSettingDefault(XYPlotArea2D.GRID_MAJOR_X, false);
+		getPlotArea().setSettingDefault(XYPlotArea2D.GRID_MAJOR_Y, false);
+		//getAxisRenderer(AXIS_X).setSetting(AxisRenderer.TICKS, false);
+		//getAxisRenderer(AXIS_Y).setSetting(AxisRenderer.TICKS, false);
+		getAxisRenderer(AXIS_X).setSetting(AxisRenderer.INTERSECTION,
+			-Double.MAX_VALUE);
+		getAxisRenderer(AXIS_Y).setSetting(AxisRenderer.INTERSECTION,
+			-Double.MAX_VALUE);
+
+		// Generate pixel data with (x, y, value)
+		Statistics stats = data.getStatistics();
+		double min = stats.get(Statistics.MIN);
+		double max = stats.get(Statistics.MAX);
+		double range = max - min;
+		DataTable pixels = new DataTable(
+			Double.class, Double.class, Double.class);
+		int i = 0;
+		for (Number value : data) {
+			double x = i%data.getColumnCount();
+			double y = -i/data.getColumnCount();
+			double v = (value.doubleValue() - min) / range;
+			pixels.add(x, y, v);
+			i++;
+		}
+
+		// Set generated data series
+		add(pixels);
+
+		getAxis(AXIS_X).setRange(0.0, data.getColumnCount());
+		getAxis(AXIS_Y).setRange(-data.getRowCount(), 0.0);
+
+		// Adjust rendering
+		PointRenderer pointRenderer = new RasterRenderer(this);
+		setLineRenderer(pixels, null);
+		setPointRenderer(pixels, pointRenderer);
+	}
+
+}
---
+++ gral-examples/src/main/java/de/erichseifert/gral/examples/rasterplot/SimpleRasterPlot.java
@@ -0,0 +1,78 @@
+/*
+ * GRAL: GRAphing Library for Java(R)
+ *
+ * (C) Copyright 2009-2011 Erich Seifert <dev[at]erichseifert.de>,
+ * Michael Seifert <michael.seifert[at]gmx.net>
+ *
+ * This file is part of GRAL.
+ *
+ * GRAL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * GRAL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GRAL.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package de.erichseifert.gral.examples.rasterplot;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import de.erichseifert.gral.data.DataTable;
+import de.erichseifert.gral.plots.RasterPlot;
+import de.erichseifert.gral.plots.colors.HeatMap;
+import de.erichseifert.gral.ui.InteractivePanel;
+import de.erichseifert.gral.util.Insets2D;
+
+
+public class SimpleRasterPlot extends JPanel {
+	/** Version id for serialization. */
+	private static final long serialVersionUID = 1L;
+
+	public SimpleRasterPlot() {
+		super(new BorderLayout());
+
+		// Create example data
+		int size = 256;
+		double f = 0.1;
+		DataTable data = new DataTable(size, Double.class);
+		for (int rowIndex = 0; rowIndex < data.getColumnCount(); rowIndex++) {
+			Number[] row = new Number[data.getColumnCount()];
+			double y = f*rowIndex;
+			for (int colIndex = 0; colIndex < row.length; colIndex++) {
+				double x = f*colIndex;
+				row[colIndex] =
+					Math.cos(Math.hypot(x - f*size/2.0, y - f*size/2.0)) *
+					Math.cos(Math.hypot(x + f*size/2.0, y + f*size/2.0));
+			}
+			data.add(row);
+		}
+
+		// Create new bar plot
+		RasterPlot plot = new RasterPlot(data);
+
+		// Format plot
+		plot.setInsets(new Insets2D.Double(20.0, 50.0, 40.0, 20.0));
+		plot.setSetting(RasterPlot.COLORS, new HeatMap());
+
+		// Add plot to Swing component
+		add(new InteractivePanel(plot));
+	}
+
+	public static void main(String[] args) {
+		SimpleRasterPlot example = new SimpleRasterPlot();
+		JFrame frame = new JFrame("GRALTest");
+		frame.getContentPane().add(example, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 600);
+		frame.setVisible(true);
+	}
+}
