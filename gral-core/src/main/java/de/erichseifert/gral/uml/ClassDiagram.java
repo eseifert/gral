711da6a30f78bd857d83092b6b07dbda5353fae5
Michael Seifert <mseifert@error-reports.org>
2013-12-14 14:06:59 +0100

    Added the missing referenced DrawableContainer "ClassDiagram".

---
+++ gral-core/src/main/java/de/erichseifert/gral/uml/ClassDiagram.java
@@ -0,0 +1,10 @@
+package de.erichseifert.gral.uml;
+
+import de.erichseifert.gral.graphics.DrawableContainer;
+import de.erichseifert.gral.graphics.TableLayout;
+
+public class ClassDiagram extends DrawableContainer {
+	public ClassDiagram() {
+		super(new TableLayout(4));
+	}
+}
