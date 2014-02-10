0eed304e89d877419f393ede0e6c5620a8d7e9df
Michael Seifert <mseifert@error-reports.org>
2014-02-10 15:47:26 +0100

    Zooming a ClassDrawable also scales the Stroke used for painting the class outline.

--- gral-uml/src/main/java/de/erichseifert/gral/uml/ClassDrawableNavigator.java
+++ gral-uml/src/main/java/de/erichseifert/gral/uml/ClassDrawableNavigator.java
@@ -1,6 +1,8 @@
 package de.erichseifert.gral.uml;
 
+import java.awt.BasicStroke;
 import java.awt.Font;
+import java.awt.Stroke;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -11,6 +13,7 @@ public class ClassDrawableNavigator extends DrawableContainerNavigator {
 	// TODO: Make DrawableContainerNavigator generic
 	private final ClassDrawable classDrawable;
 	private final Map<Label, Font> defaultFontSizesByLabel;
+	private Stroke defaultBorderStroke;
 
 	public ClassDrawableNavigator(ClassDrawable classDrawable) {
 		super(classDrawable);
@@ -21,6 +24,8 @@ public class ClassDrawableNavigator extends DrawableContainerNavigator {
 	@Override
 	public void setZoom(double zoom) {
 		super.setZoom(zoom);
+
+		// Scale font of name label
 		Label classLabel = classDrawable.getClassName();
 		Font classLabelFont = defaultFontSizesByLabel.get(classLabel);
 		if (classLabelFont == null) {
@@ -28,5 +33,18 @@ public class ClassDrawableNavigator extends DrawableContainerNavigator {
 			defaultFontSizesByLabel.put(classLabel, classLabelFont);
 		}
 		classLabel.setFont(classLabelFont.deriveFont((float) (classLabelFont.getSize2D()*zoom)));
+
+		// Scale font of border stroke
+		Stroke classBorderStroke = defaultBorderStroke;
+		if (defaultBorderStroke == null) {
+			defaultBorderStroke = classDrawable.getBorderStroke();
+			classBorderStroke = defaultBorderStroke;
+		}
+		if (classBorderStroke instanceof BasicStroke) {
+			BasicStroke basicStroke = (BasicStroke) classBorderStroke;
+			float lineWidth = basicStroke.getLineWidth();
+			Stroke strokeNew = new BasicStroke((float) (lineWidth*zoom));
+			classDrawable.setBorderStroke(strokeNew);
+		}
 	}
 }
