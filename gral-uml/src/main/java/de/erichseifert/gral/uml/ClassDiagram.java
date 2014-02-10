e049f6e0d675dec69672cf10bed6a36508db022b
Michael Seifert <mseifert@error-reports.org>
2014-02-10 12:51:28 +0100

    ClassDiagram is now Navigable.

--- gral-uml/src/main/java/de/erichseifert/gral/uml/ClassDiagram.java
+++ gral-uml/src/main/java/de/erichseifert/gral/uml/ClassDiagram.java
@@ -2,9 +2,19 @@ package de.erichseifert.gral.uml;
 
 import de.erichseifert.gral.graphics.DrawableContainer;
 import de.erichseifert.gral.graphics.TableLayout;
+import de.erichseifert.gral.navigation.Navigable;
+import de.erichseifert.gral.navigation.Navigator;
 
-public class ClassDiagram extends DrawableContainer {
+public class ClassDiagram extends DrawableContainer implements Navigable {
+	private final Navigator navigator;
+
 	public ClassDiagram() {
 		super(new TableLayout(4));
+		navigator = new ClassDiagramNavigator(this);
+	}
+
+	@Override
+	public Navigator getNavigator() {
+		return navigator;
 	}
 }
