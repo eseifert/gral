17b4cfbce36b4ab78adf765901f9f3c1c2cc638a
Michael Seifert <mseifert@error-reports.org>
2014-02-10 16:58:56 +0100

    Removed preferred size from ClassDiagram.

--- gral-uml/src/main/java/de/erichseifert/gral/uml/ClassDiagram.java
+++ gral-uml/src/main/java/de/erichseifert/gral/uml/ClassDiagram.java
@@ -3,18 +3,12 @@ package de.erichseifert.gral.uml;
 import de.erichseifert.gral.graphics.DrawableContainer;
 import de.erichseifert.gral.navigation.Navigable;
 import de.erichseifert.gral.navigation.Navigator;
-import de.erichseifert.gral.util.Dimension2D;
 
 public class ClassDiagram extends DrawableContainer implements Navigable {
 	private final Navigator navigator;
 
 	public ClassDiagram() {
 		navigator = new DrawableContainerNavigator(this);
-	}
-
-	@Override
-	public Dimension2D getPreferredSize() {
-		return new Dimension2D.Double(Double.MAX_VALUE, Double.MAX_VALUE);
 	}
 
 	@Override
