82c639f7cc9eec4129c1d0f79a0a84762b3aaf76
Michael Seifert <mseifert@error-reports.org>
2014-03-23 16:22:43 +0100

    Added a test for EditableLabel which checks that the label can be drawn with an empty string as text.

--- gral-core/src/test/java/de/erichseifert/gral/graphics/EditableLabelTest.java
+++ gral-core/src/test/java/de/erichseifert/gral/graphics/EditableLabelTest.java
@@ -1,17 +1,45 @@
 package de.erichseifert.gral.graphics;
 
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class EditableLabelTest {
 	private static EditableLabel label;
+	private static DrawingContext drawingContext;
+
+	@BeforeClass
+	public static void setUpBeforeClass() {
+		Image image = new BufferedImage(800, 600, BufferedImage.TYPE_INT_RGB);
+		drawingContext = new DrawingContext((Graphics2D) image.getGraphics());
+	}
 
 	@Before
 	public void setUp() throws Exception {
 		label = new EditableLabel();
+	}
+
+	@Test
+	public void testCreation() {
+		EditableLabel label = new EditableLabel();
+	}
+
+	@Test
+	public void testDrawEmptyLabel() {
+		label.setText("");
+
+		// Ensure that the label can be drawn while not being edited
+		label.draw(drawingContext);
+		// ... and while being edited
+		label.setEdited(true);
+		label.draw(drawingContext);
 	}
 
 	@Test
