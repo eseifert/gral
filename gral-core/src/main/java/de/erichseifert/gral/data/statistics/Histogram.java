19baf86856fabd89e999de1e6696e164f1fdd5d2
Michael Seifert <mseifert@error-reports.org>
2016-04-07 14:17:48 +0200

    Removed duplicate code for bin filling in Histogram.

--- gral-core/src/main/java/de/erichseifert/gral/data/statistics/Histogram.java
+++ gral-core/src/main/java/de/erichseifert/gral/data/statistics/Histogram.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Iterator;
 
 public class Histogram implements Iterable<Integer> {
+	private Iterable<Comparable<?>> data;
 	private Number[] breaks;
 	private Integer[] bins;
 	private int binCount;
@@ -13,36 +14,35 @@ public class Histogram implements Iterable<Integer> {
 			throw new IllegalArgumentException("Invalid bucket count: " + binCount +
 					" A histogram requires a positive bucket count.");
 		}
+		this.data = data;
 		this.binCount = binCount;
 		bins = new Integer[binCount];
 		Arrays.fill(bins, new Integer(0));
 
+		this.breaks = new Number[binCount + 1];
 		Statistics statistics = new Statistics(data);
 		double minValue = statistics.get(Statistics.MIN);
 		double maxValue = statistics.get(Statistics.MAX);
 		double range = maxValue - minValue;
-		double binWidth = range /binCount;
-		for (Comparable<?> value : data) {
-			if (!(value instanceof Number)) {
-				continue;
-			}
-			for (int binIndex = 0; binIndex < bins.length; binIndex++) {
-				double lowerBinLimit = minValue + binIndex*binWidth;
-				double upperBinLimit = minValue + (binIndex + 1)*binWidth;
-				double doubleValue = ((Number) value).doubleValue();
-				if (doubleValue >= lowerBinLimit && doubleValue < upperBinLimit) {
-					bins[binIndex]++;
-				}
-			}
+		double binWidth = range/binCount;
+		for (int breakIndex = 0; breakIndex < breaks.length; breakIndex++) {
+			breaks[breakIndex] = minValue + breakIndex*binWidth;
 		}
+
+		computeDistribution();
 	}
 
 	public Histogram(Iterable<Comparable<?>> data, Number... breaks) {
+		this.data = data;
 		this.breaks = breaks;
 		binCount = breaks.length - 1;
 		bins = new Integer[binCount];
 		Arrays.fill(bins, new Integer(0));
 
+		computeDistribution();
+	}
+
+	private void computeDistribution() {
 		for (Comparable<?> value : data) {
 			if (!(value instanceof Number)) {
 				continue;
